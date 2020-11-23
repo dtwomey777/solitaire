bca7e8e82ceb28c2bf87a08b9178e22849789c8b
dtwomey777 <47228931+dtwomey777@users.noreply.github.com>
2020-11-22 21:16:46 -0500

    added main java file

---
+++ Solitaire.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.util.Scanner; 
+
+/**
+ * AP Computer Science A Solitaire Encryption Project.
+ * 
+ * 
+ * @authors Declan Twomey, Amy Wang, Bill Fei
+ *
+ */
+
+public class Solitaire {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, world.");
+
+	}
+	
+	//Bill: Sorting Algorithm
+	//Declan: Encryption
+	//Amy: User Interface
+	
+
+}
